e583a637a8622c272b0ed2c133f90fce3bbbdb77
Dystymic <didorenfurm@hotmail.com>
2024-09-28 19:34:53 +0200

    Hard gewerkt

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+var button = document.querySelector("header nav button");
+
+var contentMenu = document.querySelector("header nav ul")
+
+button.onclick = hierIsMenu
+
+function hierIsMenu() {
+    contentMenu.classList.toggle("toonMenu");
+}
